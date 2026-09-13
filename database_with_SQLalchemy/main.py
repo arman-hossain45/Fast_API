7fb038c1007546e5db1a9d5e215714900ea9ece9
arman-hossain45 <freedomfighterit588@gmail.com>
2026-09-13 23:51:39 +0600

    project

--- database_with_SQLalchemy/main.py
+++ database_with_SQLalchemy/main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Depends
+from sqlalchemy.orm import Session
+from  typing import Annotated
 import models
-from database import engine
+from database import engine,sessionlocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+# open the database
+
+def get_db():
+    db = sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
+@app.get('/')
+def read_todos(db : Annotated[Session,Depends(get_db)]):
+    db.query(Todos).all()
